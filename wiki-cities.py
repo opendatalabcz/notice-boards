61bf17d18652d580a18508b2c8cbc29416e8cd1c
Bc. Mark Sobolev <sobolma1@fit.cvut.cz>
2019-03-20 18:14:12 +0200

    extract cities' official desks

--- wiki-cities.py
+++ wiki-cities.py
@@ -9,9 +9,19 @@ class CitySpider(scrapy.Spider):
 
     def parse(self, response):
         for city in response.css('table>tbody>tr>td>ul>li>a::attr(href)'):
-            yield response.follow("https://cs.wikipedia.org/{}".format(city.get()), self.parse_city_page)
+            yield response.follow("https://cs.wikipedia.org/{}".format(city.get()), self.extract_city_page)
 
-    def parse_city_page(self, response):
+    def extract_city_page(self, response):
         for td in response.css('table.infobox>tbody>tr>td'):
             if 'web:' in td.get():
-                yield {'web': td.css('span.url>a::attr(href)').get()}
+                yield response.follow(td.css('span.url>a::attr(href)').get(),
+                                      self.extract_official_board)
+
+    def extract_official_board(self, response):
+        result = {'city-web': response.request.url, 'desk': []}
+
+        for link in response.css('a'):
+            if re.search('deska', link.get(), re.IGNORECASE):
+                result['desk'].append(link.css('::attr(href)').get())
+
+        yield result
