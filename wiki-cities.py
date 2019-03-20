e196b76070ae5d6ae18017492eb0a6a336bb08b2
Bc. Mark Sobolev <sobolma1@fit.cvut.cz>
2019-03-20 17:30:03 +0200

    extract individiual city official webs

--- wiki-cities.py
+++ wiki-cities.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 class CitySpider(scrapy.Spider):
@@ -6,6 +7,11 @@ class CitySpider(scrapy.Spider):
     start_urls = [
         'https://cs.wikipedia.org/wiki/Seznam_m%C4%9Bst_v_%C4%8Cesku']
 
-    def parse(self, responce):
-        for city in responce.css('table>tbody>tr>td>ul>li>a::attr(href)'):
-            yield {'city-page': "https://cs.wikipedia.org/{}".format(city.get())}
+    def parse(self, response):
+        for city in response.css('table>tbody>tr>td>ul>li>a::attr(href)'):
+            yield response.follow("https://cs.wikipedia.org/{}".format(city.get()), self.parse_city_page)
+
+    def parse_city_page(self, response):
+        for td in response.css('table.infobox>tbody>tr>td'):
+            if 'web:' in td.get():
+                yield {'web': td.css('span.url>a::attr(href)').get()}
