cc24595e4e3d4dc5c121da43251a6cae12803a7f
Bc. Mark Sobolev <sobolma1@fit.cvut.cz>
2019-04-28 19:28:52 +0200

    create folder for each city web

--- download-documents.py
+++ download-documents.py
@@ -1,46 +1,38 @@
 import scrapy
 import re
 import json
+import os
 
 class DocumentSpider(scrapy.Spider):
-    name = 'cities'
-    start_urls = [
-        'https://cs.wikipedia.org/wiki/Seznam_m%C4%9Bst_v_%C4%8Cesku']
+    name = 'documents'
+    # start_urls = [
+    #     'https://cs.wikipedia.org/wiki/Seznam_m%C4%9Bst_v_%C4%8Cesku']
 
     def parse(self, response):
-        with open('helpful/desks.json') as data:
-            cities = json.load(data)
+        folder = 'https://www.abertamy.eu/'.replace('/','-').replace(':','').replace('--','-')
 
-        # for city in cities[:10]:
-        #     for desk in city['desk']:
-        #         link = ''
+        print('dir: ' + folder)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
 
-        #         if 'http' in desk or 'https' in desk or 'www' in desk:
-        #             link = desk
-        #         else:
-        #             prefix = city['city-web'][:-1] if city['city-web'][-1] == '/' else city['city-web']
-        #             link = prefix + desk 
-                    
-        #         print(link)
-        yield response.follow('https://www.abertamy.eu/uredni-deska/', self.download_documents)
+        yield response.follow('https://www.abertamy.eu/uredni-deska/', self.download_documents, meta={'folder' : folder})
 
     def download_documents(self, response):
         for h2 in response.css('div>h2.boardHeading'):
             for a in h2.css('a::attr(href)'):
-                # print(a.get())
-                yield response.follow(a.get(), self.download_file)
+                yield response.follow(a.get(), self.download_file, meta=response.meta)
 
     def download_file(self, response):
         for a in response.css('a'):
             if 'download' in a.css('::attr(href)').get():
-                # print(a.css('::attr(href)').get())
-                yield response.follow(a.css('::attr(href)').get(), self.save_pdf)
+                yield response.follow(a.css('::attr(href)').get(), self.save_pdf, meta=response.meta)
 
     def save_pdf(self, response):
-        folder = 'https://www.abertamy.eu/'.replace('/','-')
-        
-        path = folder + response.url.split('/')[-1].split('?')[0] + '.pdf'
+        folder = response.meta.get('folder')
+
+        path = folder + '/' + response.url.split('/')[-1].split('?')[0] + '.pdf'
         self.logger.info('Saving PDF %s', path)
+
         with open(path, 'wb') as f:
             f.write(response.body)
 
