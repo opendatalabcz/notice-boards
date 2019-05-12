a36ae06a2e4063b078b1cac5b85545996fb82348
Bc. Mark Sobolev <sobolma1@fit.cvut.cz>
2019-05-12 23:21:12 +0200

    prepare to report

--- download-documents.py
+++ download-documents.py
@@ -2,37 +2,66 @@ import scrapy
 import re
 import json
 import os
+import urllib.request
+
 
 class DocumentSpider(scrapy.Spider):
     name = 'documents'
-    # start_urls = [
-    #     'https://cs.wikipedia.org/wiki/Seznam_m%C4%9Bst_v_%C4%8Cesku']
+    start_urls = [
+        'https://cs.wikipedia.org/wiki/Seznam_m%C4%9Bst_v_%C4%8Cesku']
 
     def parse(self, response):
-        folder = 'https://www.abertamy.eu/'.replace('/','-').replace(':','').replace('--','-')
+        with open('helpful/desks.json') as data:
+            cities = json.load(data)
 
-        print('dir: ' + folder)
-        if not os.path.exists(folder):
-            os.makedirs(folder)
+        for i in range(0, 10):
+            folder = cities[i]['city-web'].split('.')[1].replace(
+                '/', '-').replace(':', '').replace('--', '-')
 
-        yield response.follow('https://www.abertamy.eu/uredni-deska/', self.download_documents, meta={'folder' : folder})
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
+            yield response.follow(cities[i]['city-web'], self.get_desk, meta={'folder': folder, 'desk': cities[i]['desk'][0]})
+
+    def get_desk(self, response):
+        yield response.follow(response.meta.get('desk'), self.download_documents, meta=response.meta)
 
     def download_documents(self, response):
-        for h2 in response.css('div>h2.boardHeading'):
-            for a in h2.css('a::attr(href)'):
-                yield response.follow(a.get(), self.download_file, meta=response.meta)
+        file_indicator = ['download', 'pdf',
+                          'oznámení o', 'dokument', 'document', 'file']
+        ignore_indicator = ['.wmv']
 
-    def download_file(self, response):
+        document_id = 0
+
         for a in response.css('a'):
-            if 'download' in a.css('::attr(href)').get():
+            print('a: ' + a.get())
+            if any(word in a.get().lower() for word in ignore_indicator):
+                continue
+
+            if any(word in a.get().lower() for word in file_indicator):
+                print('href: ' + a.css('::attr(href)').get())
+                document_id = document_id + 1
+                response.meta['document_id'] = document_id
                 yield response.follow(a.css('::attr(href)').get(), self.save_pdf, meta=response.meta)
 
     def save_pdf(self, response):
-        folder = response.meta.get('folder')
+        print('content-type:' + response.headers.get('content-type').decode('utf-8'))
 
-        path = folder + '/' + response.url.split('/')[-1].split('?')[0] + '.pdf'
-        self.logger.info('Saving PDF %s', path)
+        if 'pdf' in response.headers.get('content-type').decode('utf-8'):
+            folder = response.meta.get('folder')
 
-        with open(path, 'wb') as f:
-            f.write(response.body)
+            path = folder + '/' + \
+                folder + str(response.meta.get('document_id'))
 
+            if path[-4:] != '.pdf':
+                path = path + '.pdf'
+
+            self.logger.info('Saving PDF %s', path)
+
+            with open(path, 'wb') as f:
+                f.write(response.body)
+
+        if 'html' in response.headers.get('content-type').decode('utf-8'):
+            print('url: ' + response.request.url)
+            self.download_documents(response)
+            yield response.follow(response.request.url, self.download_documents, meta=response.meta)
--- helpful/iterate-cities.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('cities.json', 'r') as data:
-    cities = json.load(data)
-
-for city in cities:
-    print(city['web'])
